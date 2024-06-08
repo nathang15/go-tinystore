bf4df0189b7cb4d624110d15388622f3cd0f380e
Nathan Nguyen <nhanngyn15@gmail.com>
2024-06-07 20:42:08 -0400

    add put and get and benchmarks for clients

--- client/client.go
+++ client/client.go
@@ -1,6 +1,13 @@
 package client
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
 	"github.com/nathang15/go-tinystore/node"
 	"github.com/nathang15/go-tinystore/ring"
 )
@@ -18,6 +25,9 @@ type Payload struct {
 
 func InitClient(configFile string, virtualNodes int) *Client {
 	nodesInfo := node.LoadNodesConfig(configFile)
+	if len(nodesInfo.Nodes) == 0 {
+		log.Fatal("No nodes found in configuration")
+	}
 	r := ring.InitRing(virtualNodes)
 	for _, node := range nodesInfo.Nodes {
 		r.Add(node.Id, node.Host, node.Port)
@@ -25,6 +35,56 @@ func InitClient(configFile string, virtualNodes int) *Client {
 	return &Client{Info: nodesInfo, Ring: r, vNode: virtualNodes}
 }
 
-//TODO: GET
+func (c *Client) Get(key string) string {
+	nodeId := c.Ring.Get(key)
+	nodeInfo := c.Info.Nodes[nodeId]
 
-//TODO: PUT
+	resp, err := http.Get(fmt.Sprintf("http://%s:%d/get", nodeInfo.Host, nodeInfo.Port))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(body)
+}
+
+func (c *Client) Put(key string, value string) (string, error) {
+	nodeId := c.Ring.Get(key)
+	if nodeId == "" {
+		return "", fmt.Errorf("no node found for key: %s", key)
+	}
+	nodeInfo, exists := c.Info.Nodes[nodeId]
+	if !exists {
+		return "", fmt.Errorf("no node information for node ID: %s", nodeId)
+	}
+
+	payload := Payload{Key: key, Value: value}
+	b := new(bytes.Buffer)
+	json.NewEncoder(b).Encode(payload)
+
+	host := fmt.Sprintf("http://%s:%d/put", nodeInfo.Host, nodeInfo.Port)
+	req, err := http.NewRequest("POST", host, b)
+	if err != nil {
+		return "", err
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
---
+++ client/client_test.go
@@ -0,0 +1,100 @@
+package client
+
+import (
+	"strconv"
+	"sync"
+	"testing"
+)
+
+// ALL WITH 100 VNODE FOR EACH NODE
+
+func Benchmark10kConcurrentPuts(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		c := InitClient("D:/tinystore/configs/nodes.json", 100)
+		var wg sync.WaitGroup
+		for i := 0; i < 10; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for j := 1; j <= 1000; j++ {
+					v := strconv.Itoa(j)
+					c.Put(v, v)
+				}
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func Benchmark50kConcurrentPuts(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		c := InitClient("D:/tinystore/configs/nodes.json", 100)
+		var wg sync.WaitGroup
+		for i := 0; i < 50; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for j := 1; j <= 1000; j++ {
+					v := strconv.Itoa(j)
+					c.Put(v, v)
+				}
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func Benchmark100kConcurrentPuts(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		c := InitClient("D:/tinystore/configs/nodes.json", 100)
+		var wg sync.WaitGroup
+		for i := 0; i < 100; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for j := 1; j <= 1000; j++ {
+					v := strconv.Itoa(j)
+					c.Put(v, v)
+				}
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkLargeVolumeConcurrentPuts(b *testing.B) {
+	largeValue := make([]byte, 1024*1024) // 1MB value
+	for n := 0; n < b.N; n++ {
+		c := InitClient("D:/tinystore/configs/nodes.json", 100)
+		var wg sync.WaitGroup
+		for i := 0; i < 10; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for j := 1; j <= 100; j++ {
+					v := strconv.Itoa(j)
+					c.Put(v, string(largeValue))
+				}
+			}()
+		}
+		wg.Wait()
+	}
+}
+
+func BenchmarkHighIterationConcurrentPuts(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		c := InitClient("D:/tinystore/configs/nodes.json", 100)
+		var wg sync.WaitGroup
+		for i := 0; i < 10; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for j := 1; j <= 10000; j++ {
+					v := strconv.Itoa(j)
+					c.Put(v, v)
+				}
+			}()
+		}
+		wg.Wait()
+	}
+}
