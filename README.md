2ce52bd4d6c6d5f039b46f56f883c62d34e120c8
Nathan Nguyen <nhanngyn15@gmail.com>
2024-06-19 20:20:58 -0400

    update readme and update client test

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # go tinystore
 
-Lightweight distributed cache that supports REST and gRPC interfaces
+## This is for learning purposes so the implementation might not be correct or performant.
 
-command to change proto:
+### Features:
+- LRU cache
+- Consistent hashing implementation uses the concept of virtual nodes. Users can specify the virtual nodes size when initializing the consistent hash ring. 
+- Note that this is a very unfair distribution for virtual nodes size lesser than 100. The distribution becomes gradually consistent when virtual nodes size are increased, it seems most consistent if the amount of vnodes is greater than 700. See the output.txt file.
+- Bully algorithm for leader election of cluster.
+- Dynamic node can join/leave cluster and every other config in consistent hashing and leader will be updated instanteneously.
+- Therefore, it has no single point of failure as there is always guaranteed to have a leader.
+Distributed cache implementation:
+
+# CREDIT
+
+#### command to change proto:
 
 protoc service.proto --go_out=.     
 protoc --go-grpc_out=. service.proto
--- client/client_test.go
+++ client/client_test.go
@@ -8,9 +8,9 @@ import (
 
 // ALL FOR DOCKER
 
-// REST BENCHMARKS
+// REST TESTS
 func Test10kConcurrentPuts(t *testing.T) {
-	c := InitClient("../configs/nodes.json", 100)
+	c := InitClient("../configs/nodes.json", 10)
 	c.StartClusterConfigWatcher()
 	var wg sync.WaitGroup
 	var mutex sync.Mutex
@@ -35,167 +35,135 @@ func Test10kConcurrentPuts(t *testing.T) {
 	t.Logf("Cache misses: %d/10,000 (%f%%)", int(miss), miss/10000)
 }
 
-// func Benchmark50kConcurrentPuts(b *testing.B) {
-// 	for n := 0; n < b.N; n++ {
-// 		c := InitClient("../configs/nodes.json", 100)
-// 		var wg sync.WaitGroup
-// 		for i := 0; i < 50; i++ {
-// 			wg.Add(1)
-// 			go func() {
-// 				defer wg.Done()
-// 				for j := 1; j <= 1000; j++ {
-// 					v := strconv.Itoa(j)
-// 					c.Put(v, v)
-// 				}
-// 			}()
-// 		}
-// 		wg.Wait()
-// 	}
-// }
+func Test50kConcurrentPuts(t *testing.T) {
+	c := InitClient("../configs/nodes.json", 10)
+	c.StartClusterConfigWatcher()
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+	miss := 0.0
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 1; j <= 1000; j++ {
+				v := strconv.Itoa(j)
+				err := c.Put(v, v)
+				if err != nil {
+					t.Logf("Error putting key %s: %v", v, err)
+					mutex.Lock()
+					miss += 1
+					mutex.Unlock()
+				}
+			}
+		}()
+	}
+	wg.Wait()
+	t.Logf("Cache misses: %d/50,000 (%f%%)", int(miss), miss/50000)
+}
 
-// func Benchmark100kConcurrentPuts(b *testing.B) {
-// 	for n := 0; n < b.N; n++ {
-// 		c := InitClient("../configs/nodes.json", 100)
-// 		var wg sync.WaitGroup
-// 		for i := 0; i < 100; i++ {
-// 			wg.Add(1)
-// 			go func() {
-// 				defer wg.Done()
-// 				for j := 1; j <= 1000; j++ {
-// 					v := strconv.Itoa(j)
-// 					c.Put(v, v)
-// 				}
-// 			}()
-// 		}
-// 		wg.Wait()
-// 	}
-// }
-
-// func BenchmarkLargeVolumeConcurrentPuts(b *testing.B) {
+// func TestLargeVolumeConcurrentPuts(t *testing.T) {
 // 	largeValue := make([]byte, 1024*1024) // 1MB value
-// 	for n := 0; n < b.N; n++ {
-// 		c := InitClient("../configs/nodes.json", 100)
-// 		var wg sync.WaitGroup
-// 		for i := 0; i < 10; i++ {
-// 			wg.Add(1)
-// 			go func() {
-// 				defer wg.Done()
-// 				for j := 1; j <= 100; j++ {
-// 					v := strconv.Itoa(j)
-// 					c.Put(v, string(largeValue))
+// 	c := InitClient("../configs/nodes.json", 100)
+// 	c.StartClusterConfigWatcher()
+// 	var wg sync.WaitGroup
+// 	var mutex sync.Mutex
+// 	miss := 0.0
+// 	for i := 0; i < 10; i++ {
+// 		wg.Add(1)
+// 		go func() {
+// 			defer wg.Done()
+// 			for j := 1; j <= 1000; j++ {
+// 				v := strconv.Itoa(j)
+// 				err := c.Put(v, string(largeValue))
+// 				if err != nil {
+// 					t.Logf("Error putting key %s: %v", v, err)
+// 					mutex.Lock()
+// 					miss += 1
+// 					mutex.Unlock()
 // 				}
-// 			}()
-// 		}
-// 		wg.Wait()
+// 			}
+// 		}()
 // 	}
+// 	wg.Wait()
+// 	t.Logf("Cache misses: %d/10,000 (%f%%)", int(miss), miss/10000)
 // }
 
-// func BenchmarkHighIterationConcurrentPuts(b *testing.B) {
-// 	for n := 0; n < b.N; n++ {
-// 		c := InitClient("../configs/nodes.json", 100)
-// 		var wg sync.WaitGroup
-// 		for i := 0; i < 10; i++ {
-// 			wg.Add(1)
-// 			go func() {
-// 				defer wg.Done()
-// 				for j := 1; j <= 10000; j++ {
-// 					v := strconv.Itoa(j)
-// 					c.Put(v, v)
-// 				}
-// 			}()
-// 		}
-// 		wg.Wait()
-// 	}
-// }
+// GRPC TESTS
+func Test10kConcurrentGRPCPuts(t *testing.T) {
+	c := InitClient("../configs/nodes.json", 100)
+	c.StartClusterConfigWatcher()
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+	miss := 0.0
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 1; j <= 1000; j++ {
+				v := strconv.Itoa(j)
+				err := c.PutForGrpc(v, v)
+				if err != nil {
+					t.Logf("Error putting key %s: %v", v, err)
+					mutex.Lock()
+					miss += 1
+					mutex.Unlock()
+				}
+			}
+		}()
+	}
+	wg.Wait()
+	t.Logf("Cache misses: %d/10,000 (%f%%)", int(miss), miss/10000)
+}
 
-// // GRPC BENCHMARKS
-// func Benchmark10kConcurrentPutsGrpc(b *testing.B) {
-// 	for n := 0; n < b.N; n++ {
-// 		c := InitClient("../configs/nodes.json", 100)
-// 		var wg sync.WaitGroup
-// 		for i := 0; i < 10; i++ {
-// 			wg.Add(1)
-// 			go func() {
-// 				defer wg.Done()
-// 				for j := 1; j <= 1000; j++ {
-// 					v := strconv.Itoa(j)
-// 					c.PutForGrpc(v, v)
-// 				}
-// 			}()
-// 		}
-// 		wg.Wait()
-// 	}
-// }
+func Test50kConcurrentGRPCPuts(t *testing.T) {
+	c := InitClient("../configs/nodes.json", 100)
+	c.StartClusterConfigWatcher()
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+	miss := 0.0
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for j := 1; j <= 1000; j++ {
+				v := strconv.Itoa(j)
+				err := c.PutForGrpc(v, v)
+				if err != nil {
+					t.Logf("Error putting key %s: %v", v, err)
+					mutex.Lock()
+					miss += 1
+					mutex.Unlock()
+				}
+			}
+		}()
+	}
+	wg.Wait()
+	t.Logf("Cache misses: %d/50,000 (%f%%)", int(miss), miss/50000)
+}
 
-// func Benchmark50kConcurrentPutsGrpc(b *testing.B) {
-// 	for n := 0; n < b.N; n++ {
-// 		c := InitClient("../configs/nodes.json", 100)
-// 		var wg sync.WaitGroup
-// 		for i := 0; i < 50; i++ {
-// 			wg.Add(1)
-// 			go func() {
-// 				defer wg.Done()
-// 				for j := 1; j <= 1000; j++ {
-// 					v := strconv.Itoa(j)
-// 					c.PutForGrpc(v, v)
-// 				}
-// 			}()
-// 		}
-// 		wg.Wait()
-// 	}
-// }
-
-// func Benchmark100kConcurrentPutsGrpc(b *testing.B) {
-// 	for n := 0; n < b.N; n++ {
-// 		c := InitClient("../configs/nodes.json", 100)
-// 		var wg sync.WaitGroup
-// 		for i := 0; i < 100; i++ {
-// 			wg.Add(1)
-// 			go func() {
-// 				defer wg.Done()
-// 				for j := 1; j <= 1000; j++ {
-// 					v := strconv.Itoa(j)
-// 					c.PutForGrpc(v, v)
-// 				}
-// 			}()
-// 		}
-// 		wg.Wait()
-// 	}
-// }
-
-// func BenchmarkLargeVolumeConcurrentPutsGrpc(b *testing.B) {
+// func TestLargeVolumeConcurrentGRPCPuts(t *testing.T) {
 // 	largeValue := make([]byte, 1024*1024) // 1MB value
-// 	for n := 0; n < b.N; n++ {
-// 		c := InitClient("../configs/nodes.json", 100)
-// 		var wg sync.WaitGroup
-// 		for i := 0; i < 10; i++ {
-// 			wg.Add(1)
-// 			go func() {
-// 				defer wg.Done()
-// 				for j := 1; j <= 100; j++ {
-// 					v := strconv.Itoa(j)
-// 					c.PutForGrpc(v, string(largeValue))
+// 	c := InitClient("../configs/nodes.json", 100)
+// 	c.StartClusterConfigWatcher()
+// 	var wg sync.WaitGroup
+// 	var mutex sync.Mutex
+// 	miss := 0.0
+// 	for i := 0; i < 10; i++ {
+// 		wg.Add(1)
+// 		go func() {
+// 			defer wg.Done()
+// 			for j := 1; j <= 1000; j++ {
+// 				v := strconv.Itoa(j)
+// 				err := c.PutForGrpc(v, string(largeValue))
+// 				if err != nil {
+// 					t.Logf("Error putting key %s: %v", v, err)
+// 					mutex.Lock()
+// 					miss += 1
+// 					mutex.Unlock()
 // 				}
-// 			}()
-// 		}
-// 		wg.Wait()
+// 			}
+// 		}()
 // 	}
-// }
-
-// func BenchmarkHighIterationConcurrentPutsGrpc(b *testing.B) {
-// 	for n := 0; n < b.N; n++ {
-// 		c := InitClient("../configs/nodes.json", 100)
-// 		var wg sync.WaitGroup
-// 		for i := 0; i < 10; i++ {
-// 			wg.Add(1)
-// 			go func() {
-// 				defer wg.Done()
-// 				for j := 1; j <= 10000; j++ {
-// 					v := strconv.Itoa(j)
-// 					c.PutForGrpc(v, v)
-// 				}
-// 			}()
-// 		}
-// 		wg.Wait()
-// 	}
+// 	wg.Wait()
+// 	t.Logf("Cache misses: %d/10,000 (%f%%)", int(miss), miss/10000)
 // }
